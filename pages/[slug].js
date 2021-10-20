5c0e5c0a9cb4755332c0d9bc118688d17a197d7d
trai25 <trai25@gmail.com>
2021-10-20 23:08:00 +0200

    ..

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,17 +1,22 @@
-import BlogLayout from '@/layouts/blog'
+import Layout from '@/layouts/layout'
 import { getAllPosts, getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
+import { createHash } from 'crypto'
 
-const BlogPost = ({ post, blockMap }) => {
+const BlogPost = ({ post, blockMap, emailHash }) => {
   if (!post) return null
-  return <BlogLayout blockMap={blockMap} frontMatter={post}></BlogLayout>
+  return (
+    <Layout
+      blockMap={blockMap}
+      frontMatter={post}
+      emailHash={emailHash}
+      fullWidth={post.fullWidth}
+    />
+  )
 }
 
 export async function getStaticPaths () {
-  let posts = await getAllPosts()
-  posts = posts.filter(
-    post => post.status === 'Published' && post.type === 'Post'
-  )
+  const posts = await getAllPosts({ includePages: true })
   return {
     paths: posts.map(row => `${BLOG.path}/${row.slug}`),
     fallback: true
@@ -19,14 +24,17 @@ export async function getStaticPaths () {
 }
 
 export async function getStaticProps ({ params: { slug } }) {
-  let posts = await getAllPosts()
-  posts = posts.filter(
-    post => post.status === 'Published' && post.type === 'Post'
-  )
+  const posts = await getAllPosts({ includePages: true })
   const post = posts.find(t => t.slug === slug)
   const blockMap = await getPostBlocks(post.id)
+  const emailHash = createHash('md5')
+    .update(BLOG.email)
+    .digest('hex')
+    .trim()
+    .toLowerCase()
+
   return {
-    props: { post, blockMap },
+    props: { post, blockMap, emailHash },
     revalidate: 1
   }
 }
